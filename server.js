0a413bbf10038ac1ff62e6dba4cb43f2b9b110d2
Glitch (recondite-dolomite-jumper) <none>
2020-12-16 19:09:39 +0000

    🏯🌌 Checkpoint ./server.js:26224968/58

--- server.js
+++ server.js
@@ -352,7 +352,7 @@ client.unload = command => {
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
 		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
-			msg.author.sendMessage('https://media.giphy.com/media/LQ4VGDV2lkMJ4s7QeJ/giphy.gif'); 
+			msg.author.sendMessage(''); 
 		} else {
 		msg.reply('https://media.giphy.com/media/LQ4VGDV2lkMJ4s7QeJ/giphy.gif');
 		}
